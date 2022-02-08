ed9f63e49a491fa30e13c2d36f122de7e6851916
tasi788 <tdc@tdccc.com.tw>
2022-02-08 13:04:31 +0800

    ⚡️

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-const url = "https://myprivacy.uber.com/privacy/api/getEatsOrders?localeCode=en-US"
+const url = 'https://myprivacy.uber.com/privacy/api/getEatsOrders?localeCode=en-US'
 
 async function fetchOrder(uuid = null) {
-    let data = {"limit": 40, "lastWorkflowUuid": uuid}
-    let payload = {
+    const data = {limit: 40, lastWorkflowUuid: uuid}
+    const payload = {
         method: 'POST',
         credentials: 'same-origin',
         headers: {'Content-Type': 'application/json', 'x-csrf-token': 'x'},
         body: JSON.stringify(data)
     }
-    let resp = await fetch(url, payload)
+    const resp = await fetch(url, payload)
     return await resp.json()
 }
 
+(async () => {
+    let lastWorkflowUuid = null
+    let total = 0
+    let oldest = null
+    while (true) {
+        const result = await fetchOrder(lastWorkflowUuid)
+        if (result.data.orders.length === 0) {
+            break
+        }
+        lastWorkflowUuid = result.data.orders[result.data.orders.length - 1].uuid
+        oldest = result.data.orders[result.data.orders.length - 1].completionTime
+        for (const order of result.data.orders) {
+            total += order.fare
+        }
+        console.log('讀取中...')
+        console.log('目前總金額：', total)
 
-i = 0
-let lastWorkflowUuid = null
-total = 0
-while (i < 1) {
-    let result = await fetchOrder(lastWorkflowUuid)
-    if (result.data.orders.length === 0) {
-        break
     }
-    lastWorkflowUuid = result.data.orders[result.data.orders.length - 1].uuid
-    for (const order of result.data.orders) {
-        total += order.fare
-    }
-    console.log('讀取中...')
-    console.log('目前總金額：', total)
-}
-console.log('UberEats總消費金額➡️', total)
+
+    console.log('自從 ', new Date(oldest).toLocaleString())
+    console.log('UberEats 的總消費金額 ➡️', total)
+})()
